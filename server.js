1d0d818908b66cc8d8f183801ce4af81dbc6fa7e
Mark Stier <stier020@Marks-MacBook-Pro.local>
2015-10-06 21:47:19 -0500

    moved pulses out

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var glob = require("glob");
 var bodyParser = require("body-parser");
 var Application = require("smart-agg");
 var _ = require("lodash");
-var Pulse = require("./lib/pulse");
 var config = require("./config");
 var ModFetch = require("./lib/module-fetch");
 var Modules = require("./lib/modules");
@@ -14,6 +13,7 @@ var app = express();
 
 var routesFiles = glob.sync(__dirname + "/routes/*.js");
 var myApp = new Application({
+  debug:true,
   adapters:config.adapters || [],
   interfaces:config.interfaces || {}
 });
@@ -99,7 +99,3 @@ var logger = myApp.createClient("logger");
 logger.on("invoke-rule", function(rule){
   console.log(rule);
 });
-
-for(var i in config.pulses){
-  var p = new Pulse(config.pulses[i], myApp);
-}
